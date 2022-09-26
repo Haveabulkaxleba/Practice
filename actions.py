789f64f0ca4ad7e7060517c90bc4961520977c12
Maria <marykor1997@gmail.com>
2022-09-26 22:32:52 +0300

    Employees

---
+++ actions.py
@@ -0,0 +1,11 @@
+def edit_data(undifier, current, new_data):
+    file_base = open("Employees.txt","r") 
+    data_base = file_base.readlines()
+    file_base.close()
+    for line in data_base:
+        if line.startswith(undifier):
+            data = line.split(', ')
+            data[current] = new_data
+            new_line = ', '.join(data)
+    else:
+        print(" Такого сотрудника нет ")

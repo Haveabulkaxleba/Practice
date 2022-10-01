c2091ef8f2a9fe905de2c43845fda3b09c890559
Maria <marykor1997@gmail.com>
2022-10-01 14:16:43 +0300

    added all

--- actions.py
+++ actions.py
@@ -1,11 +1,145 @@
-def edit_data(undifier, current, new_data):
-    file_base = open("Employees.txt","r") 
-    data_base = file_base.readlines()
-    file_base.close()
+def add_check(surname, name, number, pos, salary):
+    whole = f'Фамилия: {surname}; Имя: {name}; Телефон: {number}; Должность: {pos}; Зарплата: {salary}\n'
+    lst = whole.split('; ')
+    with open('Employees.txt', 'r', encoding='utf-8') as e:
+        data_base = e.readlines()
+    # e = open('Employees.txt', 'a', encoding='utf-8')
     for line in data_base:
-        if line.startswith(undifier):
-            data = line.split(', ')
-            data[current] = new_data
-            new_line = ', '.join(data)
+        data = line.split('; ')
+        k = data[1:]
+        if lst == k:
+            return True
+        else:
+            break
+
+
+def add_data(surname, name, number, pos, salary):
+    whole = f'Фамилия: {surname}; Имя: {name}; Телефон: {number}; Должность: {pos}; Зарплата: {salary}\n'
+    lst = whole.split('; ')
+    with open('Employees.txt', 'r', encoding='utf-8') as e:
+        data_base = e.readlines()
+    e = open('Employees.txt', 'a', encoding='utf-8')
+    s = ''
+    last_line = data_base[-1]
+    if last_line.startswith('\n'):
+        last_l = data_base[-2]
+        last_l = last_l.split('; ')
+        lst.insert(0, str(int(last_l[0]) + 1))
+        new_line = '; '.join(lst)
+        s = f'{new_line}'
+        e.writelines(f'{s}')
     else:
-        print(" Такого сотрудника нет. ")
+        last_line1 = last_line.split('; ')
+        lst.insert(0, str(int(last_line1[0]) + 1))
+        new_line = '; '.join(lst)
+        s = f'{new_line}'
+        e.writelines(f'{s}')
+    e.close()
+
+
+def view():
+    with open('Employees.txt', 'r', encoding='utf-8') as mf:
+        read = mf.read()
+    return read
+
+
+def edit_surname(id, new_data):
+    with open('Employees.txt', 'r', encoding='utf-8') as e:
+        data_base = e.readlines()
+    e = open('Employees.txt', 'w', encoding='utf-8')
+    s = ''
+    for line in data_base:
+        if line.startswith(f'{id}'):
+            data = line.split('; ')
+            data[1] = f'Фамилия: {new_data}'
+            # data[1] = data[1].replace(f'{current}', f'{new_data}')
+            new_line = '; '.join(data)
+            s += f'{new_line}'
+        else:
+            s += f'{line}'
+    e.writelines(f'{s}')
+    e.close()
+
+
+def edit_name(id, new_data):
+    with open('Employees.txt', 'r', encoding='utf-8') as e:
+        data_base = e.readlines()
+    e = open('Employees.txt', 'w', encoding='utf-8')
+    s = ''
+    for line in data_base:
+        if line.startswith(f'{id}'):
+            data = line.split('; ')
+            data[2] = f'Имя: {new_data}'
+            new_line = '; '.join(data)
+            s += f'{new_line}'
+        else:
+            s += f'{line}'
+    e.writelines(f'{s}')
+    e.close()
+
+
+def edit_number(id, new_data):
+    with open('Employees.txt', 'r', encoding='utf-8') as e:
+        data_base = e.readlines()
+    e = open('Employees.txt', 'w', encoding='utf-8')
+    s = ''
+    for line in data_base:
+        if line.startswith(f'{id}'):
+            data = line.split('; ')
+            data[3] = f'Телефон: {new_data}'
+            # data[1] = data[1].replace(f'{current}', f'{new_data}')
+            new_line = '; '.join(data)
+            s += f'{new_line}'
+        else:
+            s += f'{line}'
+            e.writelines(f'{s}')
+    e.close()
+
+   
+def edit_position(ident, new_data):
+    with open('Employees.txt', 'r', encoding='utf-8') as e:
+        data_base = e.readlines()
+    e = open('Employees.txt', 'w', encoding='utf-8')
+    s = ''
+    for line in data_base:
+        if line.startswith(f'{ident}'):
+            data = line.split('; ')
+            data[4] = f'Должность: {new_data}'
+            # data[1] = data[1].replace(f'{current}', f'{new_data}')
+            new_line = '; '.join(data)
+            s += f'{new_line}'
+        else:
+            s += f'{line}'
+            e.writelines(f'{s}')
+    e.close()
+
+
+def delete_data(identifier):
+    with open('Employees.txt', 'r', encoding='utf-8') as e:
+        data_base = e.readlines()
+
+    with open('Employees.txt', 'w', encoding='utf-8') as f:
+        for line in data_base:
+            if line.startswith(f'{identifier}'):
+                with open('archive.txt', 'a', encoding='utf-8') as n:
+                    n.writelines(f'{line}')
+            if not line.startswith(f'{identifier}'):
+                f.write(line)
+
+
+def calc_check(ident):
+    e = open('Employees.txt', 'r', encoding='utf-8')
+    data_base = e.readlines()
+    for line in data_base:
+        if line.startswith(f'{ident}'):
+            return line
+
+
+def calc(ident, norm, worked):
+    # e = open('Employees.txt', 'r', encoding='utf-8')
+    data = calc_check(ident).split('; ')
+    lst = data[5].split()
+    num_list = [int(num) for num in filter(lambda num: num.isnumeric(), lst)]
+    sal = num_list[0]
+    result = round(sal / norm * worked, 2)
+    return str(result)
--- ui.py
+++ ui.py
@@ -1,23 +1,157 @@
 import actions as act
 
-
 def choice():
     while True:
-        text = input('Выберите действие с данными: 1 - добавить, 2 - удалить, 3 - просмотреть, 4 - редактировать, 5 - выйти: ')
+        text = input('''Выберите действие с данными:
+1 - добавить сотрудника
+2 - удалить сотрудника (при увольнении)
+3 - просмотреть
+4 - редактировать
+5 - расчитать оклад сотрудника за месяц
+6 - выйти из программы
+Ответ: ''')
         if text == '1':
-            data = input('Введите данные: Имя, Фамилия, Телефон, Должность: ')
-            act.add_data(data)
+            while True:
+                while True:
+                    surname = input('''Введите данные.
+Фамилия: ''')
+                    if surname.isalpha():
+                        break
+                    if not surname.isalpha():
+                        print('Введены некорректные символы. Повторите ввод.')
+                        continue
+                while True:
+                    name = input('Имя: ')
+                    if name.isalpha():
+                        break
+                    if not name.isalpha():
+                        print('Введены некорректные символы. Повторите ввод.')
+                        continue
+                    print('Введены некорректные символы. Повторите ввод.')
+                while True:
+                    number = input('Введите номер телефона (11 цифр без пробелов): ')
+                    if number.isnumeric() and len(number) == 11:
+                        break
+                    else:
+                        print('Номер должен состоять из 11 цифр.')
+                while True:
+                    position = input('Укажите должность: ')
+                    if position.isalpha():
+                        break
+                    else:
+                        print('Название долножсти должно состоять только из букв. Повторите ввод.')
+                while True:
+                    salary = input('Введите размер зарплаты: ')
+                    if salary.isdigit():
+                        break
+                    else:
+                        print('Используйте только цифры для указания зарплаты')
+                act.add_check(surname, name, number, position, salary)
+                if act.add_check(surname, name, number, position, salary):
+                    print('Данные о таком сотруднике уже есть в базе. Повторите ввод.')
+                elif not act.add_check(surname, name, number, position, salary):
+                    act.add_data(surname, name, number, position, salary)
+                    print('Данные были добавлены.')
+                    break
         elif text == '2':
-            identifier = input('Введите id для удаления данных: ')
-            act.remove_data(identifier)
+            ask = input('''При удалении сотрудника данные о нём будут перенесены в отдельный файл (archive.txt).
+Для удаления данных необходимо указать идентификатор сотрудника в файле.
+Если вы не знаете, какой идентификатор вам нужен, введите 1, чтобы запросить список данных с идентификаторами.
+Если вы знаете нужный идентификатор, введите 2 для продолжения.
+Ответ: ''')
+            if ask == '1':
+                print(act.view())
+            elif ask == '2':
+                identifier = input('Введите id сотрудника для удаления: ')
+                act.delete_data(identifier)
         elif text == '3':
-            act.view_data()
+            print(act.view())
         elif text == '4':
-            identifier = input('Введите id для редактирования данных: ')
-            current = input('Что меняем: 1 - Фамилия, 2 - Имя, 3 - Телефон, 4 - Должность')
-            new = input('Введите новую запись: ')
-            act.edit_data(identifier, current, new)
+            ask = input('''Для изменения данных необходимо указать идентификатор сотрудника в файле.
+Если вы не знаете, какой идентификатор вам нужен, введите 1, чтобы запросить список сотрудников с идентификаторами.
+Если вы знаете нужный идентификатор, введите 2 для продолжения.
+Ответ: ''')
+            if ask == '1':
+                print(act.view())
+            elif ask == '2':
+                while True:
+                    identifier = input('Введите id для редактирования данных: ')
+                    if identifier.isdigit():
+                        break
+                    else:
+                        print('Введены некорректные символы. Повторите ввод.')
+                choice = input('''Выберите, что будем менять:
+1 - Фамилию
+2 - Имя
+3 - Телефон
+4 - Должность
+Ответ: ''')
+                while True:
+                    if choice == '1':
+                        surname = input('Введите новую фамилию: ')
+                        if surname.isalpha():
+                            act.edit_surname(identifier, surname)
+                            print('Данные были изменены')
+                            break
+                        if not surname.isalpha():
+                            print('Введены некорректные символы. Повторите ввод.')
+                    elif choice == '2':
+                        while True:
+                            name = input('Введите новое имя: ')
+                            if name.isalpha():
+                                act.edit_name(identifier, name)
+                                print('Данные были изменены')
+                                break
+                            else:
+                                print('Введены некорректные символы. Повторите ввод.')
+                    elif choice == '3':
+                        while True:
+                            new_num = input('Введите новый номер телефона (11 цифр без пробелов): ')
+                            if new_num.isnumeric() and len(new_num) == 11:
+                                new = new_num.replace(' ', '')
+                                act.edit_number(identifier, new)
+                                print('Данные были изменены')
+                                break
+                            else:
+                                print('Номер должен состоять из 11 цифр.')
+                    elif choice == '4':
+                        pos = input('Введите название новой должности: ')
+                        if pos.isalpha():
+                            act.edit_position(identifier, pos)
+                            print('Данные были изменены')
+                            break
+                        else:
+                            print('Введены некорректные символы. Повторите ввод.')
+                    else:
+                        print('Введены некорректные символы. Повторите ввод.')
+                        break
         elif text == '5':
+            ask = input('''Для удаления данных необходимо указать идентификатор сотрудника в файле.
+Если вы не знаете, какой идентификатор вам нужен, введите 1, чтобы запросить список данных с идентификаторами.
+Если вы знаете нужный идентификатор, введите 2 для продолжения.
+Ответ: ''')
+            if ask == '1':
+                print(act.view())
+            elif ask == '2':
+                while True:
+                    identifier = input('Введите id для редактирования данных: ')
+                    if identifier.isdigit():
+                        break
+                    else:
+                        print('Введены некорректные символы. Повторите ввод.')
+                print('''Для расчёта укажите норму рабочего времени в целевом месяце в днях,
+а также количество фактически отработанных дней за месяц''')
+                try:
+                    norm = int(input('Норма рабочего времени: '))
+                    worked = int(input('Отработно дней: '))
+                    act.calc_check(identifier)
+                    if act.calc_check(identifier):
+                        print('Оклад за текущий месяц: ', act.calc(identifier, norm, worked))
+                    else:
+                        print('Сотрудника с таким идеентификатором нет.')
+                except ValueError:
+                    print('Возникла ошибка при расчёте. Необходимо указать целое число.')
+        elif text == '6':
             break
         else:
             print('Введена некорректная команда.')
